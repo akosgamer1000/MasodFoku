d08aae2ac4fa0c0cb5496d306d0821fbb8bd93f3
akos <your.email@example.com>
2024-12-09 18:56:57 +0100

    gsdfsfsdfs

--- src/main/java/hu/petrik_13e/masodfoku/HelloController.java
+++ src/main/java/hu/petrik_13e/masodfoku/HelloController.java
@@ -1,10 +1,7 @@
 package hu.petrik_13e.masodfoku;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class HelloController {
 
@@ -21,21 +18,52 @@ public class HelloController {
     @FXML
     private Button cal;
 
+
+    public Boolean check(){
+        if(m1.getText().equals("") || m2.getText().equals("") || m3.getText().equals("")){
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("nem lehetüres");
+            alert.showAndWait();
+            return Boolean.FALSE;
+        }
+        try{
+            Double.parseDouble(m1.getText());
+            Double.parseDouble(m2.getText());
+            Double.parseDouble(m3.getText());
+            return  Boolean.TRUE;
+        }catch (NumberFormatException e){
+            Alert alert=new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("számot kell megadni");
+            alert.showAndWait();
+            return  Boolean.FALSE;
+        }
+
+    }
     @FXML
     public void calon() {
-        double b=Double.parseDouble(m2.getText());
-        double a=Double.parseDouble(m1.getText());
-        double c=Double.parseDouble(m3.getText());
-        double d = b*b - 4*a*c;
+        if(check()){
+            double b=Double.parseDouble(m2.getText());
+            double a=Double.parseDouble(m1.getText());
+            double c=Double.parseDouble(m3.getText());
+            double d = b*b - 4*a*c;
 
-        if(a == 0 || d < 0) {
-            target.setText("nem oldható meg valós számok halmazán");
+            if(a == 0 || d < 0) {
+                target.setText("nem oldható meg valós számok halmazán");
+            } else if (d==0) {
+
+
+                    double x1 = (-b + Math.sqrt(d))/(2*a);
+
+                    target.setText("x1=x2="+x1);
+
+
+            } else{
+                double x1 = (-b + Math.sqrt(d))/(2*a);
+                double x2 = (-b - Math.sqrt(d))/(2*a);
+                target.setText("x1="+x1+"\n"+"x2="+x2);
+            }
         }
-        else{
-            double x1 = (-b + Math.sqrt(d))/(2*a);
-            double x2 = (-b - Math.sqrt(d))/(2*a);
-            target.setText("x1="+x1+"\n"+"x2="+x2);
-        }
+
 
 
     }
